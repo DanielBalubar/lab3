0756a696efad421a128467021966a9b4bdfe6626
DanielBalubar <152581120+DanielBalubar@users.noreply.github.com>
2024-02-27 22:27:09 +0800

    Update index.php

--- ci4/public/index.php
+++ ci4/public/index.php
@@ -1,87 +1,143 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BALUBAR.DANIEL</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+    body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
+    .w3-row-padding img {margin-bottom: 12px}
+ 
+    .w3-sidebar {width: 120px;background: #925a5a;}
+ 
+    #main {margin-left: 120px}
+    @media only screen and (max-width: 600px) {#main {margin-left: 0}}
+    </style>
+  </head>
+<body class="w3-black">
 
-// Check PHP version.
-$minPhpVersion = '7.4'; // If you update this, don't forget to update `spark`.
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION
-    );
+<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+  <img src="https://scontent.fmnl17-3.fna.fbcdn.net/v/t39.30808-6/399175238_2095668140784342_8814209987314040277_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=58uXBKgWDdQAX-rRyjJ&_nc_ht=scontent.fmnl17-3.fna&oh=00_AfC2lsdTmuWYjoZvHOaM7_O37iZAbh_jsdT1Z-y6Ua8olw&oe=65B4351F" style="width:100%">
+  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>HOME</p>
+  </a>
+  <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-user w3-xxlarge"></i>
+    <p>ABOUT</p>
+  </a>
+  <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-eye w3-xxlarge"></i>
+    <p>PHOTOS</p>
+  </a>
+  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>CONTACT</p>
+  </a>
+</nav>
 
-    exit($message);
-}
+<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+  <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+    <a href="#" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
+    <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">ABOUT</a>
+    <a href="#photos" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
+    <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
+  </div>
+</div>
 
-// Path to the front controller (this file)
-define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
+<div class="w3-padding-large" id="main">
 
-// Ensure the current directory is pointing to the front controller's directory
-if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
-    chdir(FCPATH);
-}
+  <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+    <h1 class="w3-jumbo"><span class="w3-hide-small">I'm</span> Balubar, Daniel Louis Q..</h1>
+    <p>Begginer Web Designer.</p>
+    <img src= https://scontent.fmnl4-1.fna.fbcdn.net/v/t39.30808-6/399175238_2095668140784342_8814209987314040277_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHSWY7cQiauWkQC81BHK8mzrQr1FI2mjTKtCvUUjaaNMiIkfpPRONzDnEJ4-SARf1rMaHNWEkNeg0lWgAtfMFAp&_nc_ohc=_AYktTY8xdMAX8zUb4i&_nc_ht=scontent.fmnl4-1.fna&oh=00_AfANX5TEP4GLmWD1C0dxKLj2WTSsvhpf5JaNXsVeGBn7Vg&oe=65E3AB1F alt="boy" class="w3-image" width="992" height="1108">
+  </header>
 
-/*
- *---------------------------------------------------------------
- * BOOTSTRAP THE APPLICATION
- *---------------------------------------------------------------
- * This process sets up the path constants, loads and registers
- * our autoloader, along with Composer's, loads our constants
- * and fires up an environment-specific bootstrapping.
- */
 
-// Load our paths config file
-// This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
-// ^^^ Change this line if you move your application folder
+  <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
+    <h2 class="w3-text-light-grey">About Me</h2>
+    <hr style="width:200px" class="w3-opacity">
+    <p>Hello everyone my name is Daniel Louis Q. Balubar MI221, feel free to scroll down through my website and as you scroll down feel free to reach me..
+    </p>
+    <h3 class="w3-padding-16 w3-text-light-grey">My Skills</h3>
+    <p class="w3-wide">Designing</p>
+    <div class="w3-white">
+      <div class="w3-dark-grey" style="height:28px;width:95%"></div>
+    </div>
+    <p class="w3-wide">Programming</p>
+    <div class="w3-white">
+      <div class="w3-dark-grey" style="height:28px;width:85%"></div>
+    </div>
+    <p class="w3-wide">Eating</p>
+    <div class="w3-white">
+      <div class="w3-dark-grey" style="height:28px;width:80%"></div>
+    </div><br>
+    
+    <div class="w3-row w3-center w3-padding-16 w3-section w3-light-grey">
+      <div class="w3-quarter w3-section">
+        <span class="w3-xlarge">1+</span><br>
+        Partners
+      </div>
+      <div class="w3-quarter w3-section">
+        <span class="w3-xlarge">This one</span><br>
+        Projects Done
+      </div>
+      <div class="w3-quarter w3-section">
+        <span class="w3-xlarge">8hours+</span><br>
+        Sleeping
+      </div>
+      <div class="w3-quarter w3-section">
+        <span class="w3-xlarge">5.6"</span><br>
+        height
+      </div>
+    </div>
 
-$paths = new Config\Paths();
+    <button class="w3-button w3-light-grey w3-padding-large w3-section">
+      <i class="fa fa-download"></i> 
+      <a href="https://scontent.fmnl17-3.fna.fbcdn.net/v/t1.15752-9/422624528_297846803279712_2165884191440057575_n.png?_nc_cat=110&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=KFlzfxbzSiMAX9Up7Ez&_nc_ht=scontent.fmnl17-3.fna&oh=03_AdSLLQnvthYX5AiGZk9clyVC6HQmvt3waHB3UHAvE8xA0w&oe=65D93168" download="YourResume.pdf">Download Resume</a>
+    </button>
+    
+    
+  
+  <div class="w3-padding-64 w3-content" id="photos">
+    <h2 class="w3-text-light-grey">My Photos</h2>
+    <hr style="width:200px" class="w3-opacity">
 
-// Location of the framework bootstrap file.
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+    <div class="w3-row-padding" style="margin:0 -16px">
+      <div class="w3-half">
+        <img src=https://scontent.fmnl4-2.fna.fbcdn.net/v/t39.30808-6/318301603_1873308509686974_5074119163574912345_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeE6yGiV5XIH2t_g5C79N1_7Vfo-Mi9zLYNV-j4yL3Mtg2RQv6evbfSi-1jmm2OfWb0nttSZWIiNJkr5gD6A3UAG&_nc_ohc=j-xP2RR6BDsAX8rqmC6&_nc_ht=scontent.fmnl4-2.fna&oh=00_AfAyjFJSavwAt1iN-zMRgaKdf4qDzorG7SQRAj6I5zvKBA&oe=65E1F0C9 style="width:100%">
+        <img src= https://scontent.fmnl4-6.fna.fbcdn.net/v/t39.30808-6/284504973_1728819790802514_239961091823771155_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeFxJOuli3cLfagSpdnvi4xOtQQx5iR7GyC1BDHmJHsbIEpD1MeQX1TiCXroeLU_p0h9auqUdrpktLjwBBpBR8L_&_nc_ohc=A0q-b2c-4ywAX_gNqWk&_nc_oc=AQm8s4G0Nnosf1O0oVltYGst91bsrUpCGHfYgVB73hvA1llsNHK3OzfJTLUgvCrfMSU&_nc_ht=scontent.fmnl4-6.fna&oh=00_AfDEPKtFJ3qtDynU7mZM9i2hTTpDRnB5R4XveX5KEG79BA&oe=65E2FA73 style="width:100%">
+        <img src=https://scontent.fmnl4-1.fna.fbcdn.net/v/t39.30808-6/273371209_1651831135168047_3001017171376565326_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=9c7eae&_nc_eui2=AeEZVCR8Aq4ErlhItbZZoocYxeAsRatEaHrF4CxFq0Roev0c6zd5cy9PgyeMSh9F6tEfpbliZ-ve4sSZiAXVofBH&_nc_ohc=QTL0v2ggBokAX-LXxMB&_nc_ht=scontent.fmnl4-1.fna&oh=00_AfCZ1W94woLK-95TojzzNwgFVh2Q4M8jF4hUIf-Xg11poA&oe=65E20240 style="width:100%">
+      </div>
+  </div>
 
-// Load environment settings from .env files into $_SERVER and $_ENV
-require_once SYSTEMPATH . 'Config/DotEnv.php';
-(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
 
-// Define ENVIRONMENT
-if (! defined('ENVIRONMENT')) {
-    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
-}
+  <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
+    <h2 class="w3-text-light-grey">Contact Me</h2>
+    <hr style="width:200px" class="w3-opacity">
 
-// Load Config Cache
-// $factoriesCache = new \CodeIgniter\Cache\FactoriesCache();
-// $factoriesCache->load('config');
-// ^^^ Uncomment these lines if you want to use Config Caching.
+    <div class="w3-section">
+      <p><i class="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Taguig, Upper Bicutan</p>
+      <p><i class="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Phone: +63 916 6604 883</p>
+      <p><i class="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"> </i> Email: balubarquintua@gmail.com</p>
+    </div><br>
+    <p>Send me a message:</p>
 
-/*
- * ---------------------------------------------------------------
- * GRAB OUR CODEIGNITER INSTANCE
- * ---------------------------------------------------------------
- *
- * The CodeIgniter class contains the core functionality to make
- * the application run, and does all the dirty work to get
- * the pieces all working together.
- */
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Subject" required name="Subject"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Message" required name="Message"></p>
+      <p>
+        <button class="w3-button w3-light-grey w3-padding-large" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </p>
+    </form>
 
-$app = Config\Services::codeigniter();
-$app->initialize();
-$context = is_cli() ? 'php-cli' : 'web';
-$app->setContext($context);
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is set up, it's time to actually fire
- * up the engines and make this app do its thang.
- */
-
-$app->run();
-
-// Save Config Cache
-// $factoriesCache->save('config');
-// ^^^ Uncomment this line if you want to use Config Caching.
-
-// Exits the application, setting the exit code for CLI-based applications
-// that might be watching.
-exit(EXIT_SUCCESS);
+  </div>
+</body>
+</html>
